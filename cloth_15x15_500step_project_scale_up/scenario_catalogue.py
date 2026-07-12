955a17faa851c54a2d507ba0aaff151cba38baaa
baiiXin <95574190+baiiXin@users.noreply.github.com>
2026-07-13 00:11:54 +0800

    feat(cloth15-scale): expose scenario catalogue API

---
+++ cloth_15x15_500step_project_scale_up/scenario_catalogue.py
@@ -0,0 +1,5 @@
+"""Public facade for deterministic cloth scenario catalogues."""
+from scenario_templates import *
+from scenario_geometry import *
+from scenario_builder import *
+from scenario_audit import *
